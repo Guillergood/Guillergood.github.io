e59c8c84cb8966be0336e19d18cbd243893266dc
Guillermo Bueno Vargas <Guillergood@gmail.com>
2019-07-02 13:03:24 +0200

    Actualizado
    Actualización de la página

--- README.md
+++
@@ -1,3 +0,0 @@
-# Guillergood.github.io
-
-Página Web de Guillermo Bueno Vargas
---
+++ js/navbar.js
@@ -0,0 +1,58 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+	
+		var navbarTop = document.querySelector("#navbarTop");
+		window.addEventListener("scroll", function() {
+			
+			var separadores = document.getElementsByClassName("parts");
+			var activo = navbarTop.getElementsByClassName("active");
+			var marcadores = document.getElementsByClassName("highlight");
+			var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+			if(window.innerWidth > 768){
+			  if(scrollTop > 50) { 
+		      navbarTop.classList.add("visible");
+		      navbarTop.classList.remove("hidden");
+			  } 
+			  else {
+					navbarTop.classList.remove("visible");
+					navbarTop.classList.add("hidden");
+			  }
+			}
+
+			var continua = true;
+			var i;
+			for (i = 0; i < separadores.length-1 && continua; ++i) {
+				if(separadores[i].offsetTop - scrollTop + (separadores[i+1].offsetTop - separadores[i].offsetTop) > 0 ){
+					continua = false;
+					activo[0].classList.remove("active");
+					marcadores[i].classList.add("active");
+				}
+			}
+
+			if(separadores[separadores.length-1].offsetTop - scrollTop < 150){
+				console.log(separadores[separadores.length-1].offsetTop - scrollTop);
+				console.log("ScrollTop" + scrollTop);
+
+				navbarTop.getElementsByClassName("active")[0].classList.remove("active");
+				marcadores[separadores.length-1].classList.add("active");
+			}
+
+
+			
+
+
+
+    });
+});
+
+
+function mobileMenuClicked(){
+	if(navbarTop.classList.contains("hidden")){
+      navbarTop.classList.add("visible");
+      navbarTop.classList.remove("hidden");
+  	} 
+	else {
+			navbarTop.classList.remove("visible");
+			navbarTop.classList.add("hidden");
+  	}
+}
