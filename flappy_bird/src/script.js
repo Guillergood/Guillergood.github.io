4a10b573c7a483a6fff347243b647822d11374bb
Guillermo Bueno Vargas <Guillergood@users.noreply.github.com>
2020-01-25 13:39:29 +0100

    Update script.js

--- flappy_bird/src/script.js
+++ flappy_bird/src/script.js
@@ -73,11 +73,12 @@ function showFinalScore(){
     }
 
 
-    finalScore.innerHTML = "Tu puntuacion ha sido: " + score.innerHTML + "<br>" + "Tu puntuacion total guardada: " + localStorage.score + "<br>"+ "Haz click o pulsa la barra espaciadora para empezar de nuevo<br>";
+    finalScore.innerHTML = "Tu puntuacion ha sido: " + score.innerHTML + "<br>" + "Tu puntuacion total guardada: " + localStorage.score + "<br>"+ "Haz click o pulsa la barra espaciadora para empezar de nuevo<br> + "<br><a href=\"https://github.com/Guillergood/Guillergood.github.io/tree/master/flappy_bird\">Autores</a>" ;
   }
   else {
-    finalScore.innerHTML = "Tu puntuacion ha sido: " + score.innerHTML + "<br>" + "Haz click o pulsa la barra espaciadora para empezar de nuevo<br>";
-  }
+    finalScore.innerHTML = "Tu puntuacion ha sido: " + score.innerHTML + "<br>" + "Haz click o pulsa la barra espaciadora para empezar de nuevo<br>" + "<br><a href=\"https://github.com/Guillergood/Guillergood.github.io/tree/master/flappy_bird\">Autores</a>" ;
+  }	
+	
   finalScore.style.display = "block";
 }
 
